37f4d0aea183f668e1f8230781c780638272974b
carrot-mac <songjiexiao@zju.edu.cn>
2024-01-11 09:30:50 +0800

    add joint_mimic_controller

---
+++ include/ur_sim2real/joint_mimic_controller.hpp
@@ -0,0 +1,71 @@
+/*
+ * @Description: joint_mimic_controller.hpp
+ * @version: 1.0.0
+ * @Author: Songjie Xiao
+ * @Email: songjiexiao@zju.edu.cn
+ * @Date: 2024-01-06 09:05:12
+ * @LastEditTime: 2024-01-06 09:53:47
+ */
+
+#ifndef JOINT_MIMIC_CONTROLLER_HPP_
+#define JOINT_MIMIC_CONTROLLER_HPP_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "rclcpp/time.hpp"
+#include "rclcpp/duration.hpp"
+#include "controller_interface/controller_interface.hpp"
+#include "controller_interface/helpers.hpp"
+#include "hardware_interface/loaned_command_interface.hpp"
+#include "hardware_interface/types/hardware_interface_type_values.hpp"
+#include "realtime_tools/realtime_buffer.h"
+#include "sensor_msgs/msg/joint_state.hpp"
+
+#include "joint_mimic_controller_parameters.hpp"
+
+namespace ur_sim2real
+{
+  class JointMimicController : public controller_interface::ControllerInterface
+  {
+  public:
+    JointMimicController() = default;
+    ~JointMimicController() = default;
+
+    controller_interface::InterfaceConfiguration command_interface_configuration() const override;
+
+    controller_interface::InterfaceConfiguration state_interface_configuration() const override;
+
+    controller_interface::return_type update(const rclcpp::Time &time, const rclcpp::Duration &period) override;
+
+    controller_interface::CallbackReturn on_configure(const rclcpp_lifecycle::State &previous_state) override;
+
+    controller_interface::CallbackReturn on_activate(const rclcpp_lifecycle::State &previous_state) override;
+
+    controller_interface::CallbackReturn on_deactivate(const rclcpp_lifecycle::State &previous_state) override;
+
+    controller_interface::CallbackReturn on_init() override;
+
+  protected:
+    // Parameters from ROS for JointMimicController
+    std::shared_ptr<joint_mimic_controller::ParamListener> param_listener_;
+    joint_mimic_controller::Params params_;
+
+    size_t num_joints_;
+    std::vector<std::string> joint_names_;
+    std::vector<std::reference_wrapper<hardware_interface::LoanedCommandInterface>> position_command_interfaces_;
+    std::vector<double> position_cmd_;
+
+    // subscriber for joint states of the simulated robot
+    rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_state_sub_;
+
+    // realtime buffers for joint state messages
+    realtime_tools::RealtimeBuffer<std::shared_ptr<sensor_msgs::msg::JointState>> joint_state_buffer_;
+    std::shared_ptr<sensor_msgs::msg::JointState> joint_state_msg_;
+
+  }; // class JointMimicController
+
+} // namespace ur_sim2real
+
+#endif // JOINT_MIMIC_CONTROLLER_HPP_
---
+++ src/joint_mimic_controller.cpp
@@ -0,0 +1,170 @@
+/*
+ * @Description: joint_mimic_controller.cpp
+ * @version: 1.0.0
+ * @Author: Songjie Xiao
+ * @Email: songjiexiao@zju.edu.cn
+ * @Date: 2024-01-06 09:05:12
+ * @LastEditTime: 2024-01-06 09:53:47
+ */
+
+#include "ur_sim2real/joint_mimic_controller.hpp"
+
+namespace
+{
+  const rclcpp::Logger LOGGER = rclcpp::get_logger("ur_sim2real::JointMimicController");
+}
+
+namespace ur_sim2real
+{
+  controller_interface::CallbackReturn JointMimicController::on_init()
+  {
+    try
+    {
+      // Create the parameter listener and get the parameters
+      param_listener_ = std::make_shared<joint_mimic_controller::ParamListener>(get_node());
+      params_ = param_listener_->get_params();
+
+      RCLCPP_INFO(LOGGER, "Loading JointMimicController with tf_prefix: %s",
+                  params_.tf_prefix.c_str());
+    }
+    catch (std::exception &e)
+    {
+      fprintf(stderr, "Exception thrown during init stage with message: %s \n", e.what());
+      return controller_interface::CallbackReturn::ERROR;
+    }
+
+    return controller_interface::CallbackReturn::SUCCESS;
+  }
+
+  controller_interface::InterfaceConfiguration JointMimicController::command_interface_configuration() const
+  {
+    controller_interface::InterfaceConfiguration config;
+    config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
+
+    config.names.reserve(num_joints_);
+    for (const auto &joint_name : joint_names_)
+    {
+      config.names.emplace_back(joint_name + std::string("/position"));
+    }
+
+    return config;
+  }
+
+  controller_interface::InterfaceConfiguration JointMimicController::state_interface_configuration() const
+  {
+    return controller_interface::InterfaceConfiguration{controller_interface::interface_configuration_type::NONE};
+  }
+
+  controller_interface::CallbackReturn
+  JointMimicController::on_configure(const rclcpp_lifecycle::State & /*previous_state*/)
+  {
+    if (param_listener_->is_old(params_))
+    {
+      params_ = param_listener_->get_params();
+    }
+    joint_names_ = params_.joints;
+    if (joint_names_.empty())
+    {
+      RCLCPP_ERROR(LOGGER, "No joint names specified in ROS2 parameters");
+      return controller_interface::CallbackReturn::ERROR;
+    }
+    num_joints_ = joint_names_.size();
+    position_cmd_.resize(num_joints_);
+
+    if (!params_.tf_prefix.empty())
+    {
+      for (auto &joint_name : joint_names_)
+      {
+        joint_name = params_.tf_prefix + joint_name;
+      }
+    }
+    if (params_.joint_state_topic.empty())
+    {
+      RCLCPP_ERROR(LOGGER, "Joint state topic is empty!");
+      return controller_interface::CallbackReturn::ERROR;
+    }
+
+    try
+    {
+      auto joint_state_callback =
+          [this](const std::shared_ptr<sensor_msgs::msg::JointState> msg)
+      { joint_state_buffer_.writeFromNonRT(msg); };
+      joint_state_sub_ = get_node()->create_subscription<sensor_msgs::msg::JointState>(
+          params_.joint_state_topic, rclcpp::SystemDefaultsQoS(), joint_state_callback);
+    }
+    catch (const std::exception &e)
+    {
+      fprintf(
+          stderr, "Exception thrown during subscriber creation at configure stage with message : %s \n",
+          e.what());
+      return controller_interface::CallbackReturn::ERROR;
+    }
+
+    return controller_interface::CallbackReturn::SUCCESS;
+  }
+
+  controller_interface::CallbackReturn
+  JointMimicController::on_activate(const rclcpp_lifecycle::State & /*previous_state*/)
+  {
+
+    if (!controller_interface::get_ordered_interfaces(command_interfaces_,
+                                                      joint_names_,
+                                                      "position",
+                                                      position_command_interfaces_))
+    {
+      RCLCPP_ERROR(LOGGER, "Expected %zu command interfaces, got %zu.",
+                   joint_names_.size(),
+                   position_command_interfaces_.size());
+      return controller_interface::CallbackReturn::ERROR;
+    }
+
+    return controller_interface::CallbackReturn::SUCCESS;
+  }
+
+  controller_interface::CallbackReturn
+  JointMimicController::on_deactivate(const rclcpp_lifecycle::State & /*previous_state*/)
+  {
+    position_command_interfaces_.clear();
+    return controller_interface::CallbackReturn::SUCCESS;
+  }
+
+  controller_interface::return_type JointMimicController::update(const rclcpp::Time & /*time*/,
+                                                                 const rclcpp::Duration & /*period*/)
+  {
+    // read joint state from buffer
+    joint_state_msg_ = *joint_state_buffer_.readFromRT();
+    if (!joint_state_msg_.get())
+    {
+      RCLCPP_ERROR(LOGGER, "No joint state message received");
+      return controller_interface::return_type::ERROR;
+    }
+
+    // write joint state to command interfaces
+    for (size_t i = 0; i < num_joints_; ++i)
+    {
+      const auto &joint_name = joint_names_[i];
+      const auto &joint_state = joint_state_msg_->name;
+      const auto &joint_position = joint_state_msg_->position;
+
+      // find the index of the joint in the joint state message
+      auto it = std::find(joint_state.begin(), joint_state.end(), joint_name);
+      if (it == joint_state.end())
+      {
+        RCLCPP_ERROR(LOGGER, "Joint %s not found in joint state message", joint_name.c_str());
+        return controller_interface::return_type::ERROR;
+      }
+      const auto joint_index = std::distance(joint_state.begin(), it);
+
+      // write joint state to command interfaces
+      position_cmd_[i] = joint_position[joint_index];
+      position_command_interfaces_[i].get().set_value(position_cmd_[i]);
+    }
+    
+    return controller_interface::return_type::OK;
+  }
+
+} // namespace ur_sim2real
+
+#include "pluginlib/class_list_macros.hpp"
+
+PLUGINLIB_EXPORT_CLASS(ur_sim2real::JointMimicController, controller_interface::ControllerInterface)
